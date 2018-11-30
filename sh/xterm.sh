0b09ac0ee8b66995bf721b7b2de35b4f54bdc098
Hajime Edakawa <hajime@hpz220.my.domain>
2018-11-30 10:42:31 +0900

    Initial revision

---
+++ sh/xterm.sh
@@ -0,0 +1,34 @@
+#!/bin/ksh
+
+# xterm.sh - to spawn a new terminal for use with cwm
+
+# Require Packages:
+#   transset-df-6p3.tgz
+#   hermit-font-1.21p1.tgz
+#   mixfont-mplus-ipa-20060520p7.tgz
+
+#-------------------------------------------------------------------------------
+_OPACITY=0.75
+_FONT_SIZE=12
+_FG_COLOR=white
+_BG_COLOR=black
+
+#-------------------------------------------------------------------------------
+_US_FONT=Hermit:style=medium
+_JP_FONT=M+1P+IPAG:style=regular
+
+fc-list | grep -q ${_US_FONT}
+[[ $? -eq 1 ]] && _US_FONT=""
+fc-list | grep -q ${_JP_FONT}
+[[ $? -eq 1 ]] && _JP_FONT=""
+
+#-------------------------------------------------------------------------------
+_TITLE="XTerm_$$"
+_PROGRAM="exec ksh -c 'tmux -2'"
+
+pgrep -q xcompmgr
+[[ $? -eq 0 ]] && _PROGRAM="transset-df ${_OPACITY} --name ${_TITLE} \
+                      --no-regex >/dev/null; ${_PROGRAM}"
+#-------------------------------------------------------------------------------
+xterm -fa ${_US_FONT} -fd ${_JP_FONT} -fg ${_FG_COLOR} -bg ${_BG_COLOR} \
+    -fs ${_FONT_SIZE} -title ${_TITLE} +sb -e "${_PROGRAM}"
